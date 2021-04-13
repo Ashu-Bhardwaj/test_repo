b22592a7dea35dcf2ed10d0748866930f92797a5
Ashu Bhardwaj <ashubhardwaj1201@gmail.com>
2021-04-13 10:03:20 +0530

    job trigger

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -34,9 +34,9 @@ public class AppTest
     {
         App.main(null);
         try {
-            assertEquals("Hello World from the git!" + System.getProperty("line.separator"), outContent.toString());
+            assertEquals("Hello World!" + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
-            fail("\"message\" is not \"Hello World from the git!\"");
+            fail("\"message\" is not \"Hello World!\"");
         }
     }
 
